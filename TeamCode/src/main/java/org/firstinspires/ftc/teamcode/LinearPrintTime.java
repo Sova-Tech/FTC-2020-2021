7f1cde3c6ecc9e2acec48fa08202837279809da9
Teo <teodor.ruse@yahoo.com>
2021-01-31 18:14:33 +0200

    because maybe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearPrintTime.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearPrintTime.java
@@ -50,7 +50,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-//help;
+
 
 @TeleOp(name="LinearPrintTime", group="Linear Opmode")
 //@Disabled
